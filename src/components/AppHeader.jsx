5a427c34c03b62ac0b756801c339c665bcffd63c
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-27 15:29:19 +0100

    add Form to control Enter input

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,12 +1,30 @@
 export default function AppHeader({ search, handleSearch, setSearch }) {
     return (
-
         <div className="container-fluid bg-dark">
-            <header className="d-flex  align-items-center p-3">
-                <a href="#" className="text-danger flex-grow-1 h1 text-decoration-none">BoolFlix</a>
-                <input className="mx-5" type="text" value={search} onChange={setSearch}></input>
-                <button className="btn btn-danger px-5" onClick={handleSearch} >Cerca</button>
+            <header className="d-flex align-items-center p-3">
+                <a href="#" className="text-danger flex-grow-1 h1 text-decoration-none">
+                    BoolFlix
+                </a>
+                <form
+                    className="d-flex align-items-center"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSearch();
+                    }}>
+                    <input
+                        placeholder="Cosa cerchiamo?..."
+                        className="form-control mx-2"
+                        type="text"
+                        value={search}
+                        onChange={setSearch} 
+                    />
+                    <button
+                        type="submit"
+                        className="btn btn-danger px-5">
+                        Cerca
+                    </button>
+                </form>
             </header>
         </div>
-    )
+    );
 }
--- src/components/AppHome.jsx
+++ src/components/AppHome.jsx
@@ -1,11 +1,25 @@
-export default function AppHome({search, setSearch, handleSearch}) {
+export default function AppHome({ search, setSearch, handleSearch }) {
     return (
         <main>
-        <div className="container text-center text-white">
-        <h1 className="title text-white text-center my-5">Benvenuto su BoolFlix, la tua applicazione di ricerca di fiducia.</h1>
-        <h2 className="subtitle">Qui potrai cercare tramite il nostro catalogo costantemente aggiornato, i migliori film del momento. Prova tu stesso! </h2>
-        <input type="text" value={search} onChange={setSearch} placeholder="Cosa cerchiamo?..." className="input-field mx-3 my-5"></input> <button onClick={handleSearch} className="btn btn-danger">Cerca</button>
-        </div>
-        </main>
+            <div className="container text-center text-white">
+                <h1 className="title text-white text-center my-5">Benvenuto su BoolFlix, la tua applicazione di ricerca di fiducia.</h1>
+                <h2 className="subtitle">Qui potrai cercare tramite il nostro catalogo costantemente aggiornato, i migliori film del momento. Prova tu stesso! </h2>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSearch();
+                }}>
+                    <input type="text"
+                        value={search}
+                        onChange={setSearch}
+                        placeholder="Cosa cerchiamo?..."
+                        className="input-field mx-3 my-5">
+                    </input>
+                    <button onClick={handleSearch}
+                        className="btn btn-danger">
+                        Cerca
+                    </button>
+                </form>
+            </div>
+        </main >
     )
 }
